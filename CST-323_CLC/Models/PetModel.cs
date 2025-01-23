4fffe5d6b3ec4bdd5294bc4589b85720ae843ea6
Alex <av2o26@outlook.com>
2025-01-23 10:50:03 -0700

    Got some basic starting code in

---
+++ CST-323_CLC/Models/PetModel.cs
@@ -0,0 +1,37 @@
+﻿namespace CST_323_CLC.Models
+{
+    public class PetModel
+    {
+        // Properties
+        public String Species { get; set; }
+        public String Breed { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public double Price { get; set; }
+        public bool AdoptionStatus { get; set; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public PetModel() {}
+
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="species"></param>
+        /// <param name="breed"></param>
+        /// <param name="name"></param>
+        /// <param name="description"></param>
+        /// <param name="price"></param>
+        /// <param name="adoptionStatus"></param>
+        public PetModel(string species, string breed, string name, string description, double price, bool adoptionStatus)
+        {
+            Species = species;
+            Breed = breed;
+            Name = name;
+            Description = description;
+            Price = price;
+            AdoptionStatus = adoptionStatus;
+        }
+    }
+}
