37642021fd4fa76ee0f308daaf5a8e55196408b0
CJones001 <147557670+CJones001@users.noreply.github.com>
2025-02-10 23:44:45 -0700

    Made it to where a new user can be created and trying to make it so that you have to log in

--- CST-323_CLC/Controllers/UserController.cs
+++ CST-323_CLC/Controllers/UserController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CST_323_CLC.Models;
+using CST_323_CLC.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace CST_323_CLC.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserService userService;
+
+        public UserController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +22,29 @@ namespace CST_323_CLC.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        public IActionResult AddUser(UserModel user)
+        {
+            user.Id = ObjectId.GenerateNewId().ToString();
+
+            userService.CreateUser(user);
+
+            return RedirectToAction("Index", "Login");
+        }
+
+        public IActionResult CheckCredentials(string username, string password)
+        {
+            UserModel foundUser = userService.FindUser(username);
+
+            if (foundUser.Password == password)
+            {
+                return RedirectToAction("Index", "Pet");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
