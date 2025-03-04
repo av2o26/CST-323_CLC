b8ff38ea1f11e53c277a82d44ff2beeffef6eb62
CJones001 <147557670+CJones001@users.noreply.github.com>
2025-03-04 09:10:25 -0700

    Revert "Copied All of the changes made from the other branch that GitHub refused to update automatically in this branch"
    This reverts commit aa56d3fe5cd5f76b35ec7cb0056ae6f6a040404d.

--- CST-323_CLC/Services/Utilities/UserDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CST_323_CLC.Services.Utilities
-{
-    public class UserDatabaseSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string CollectionName { get; set; } = null!;
-    }
-}
