ac3d7cdfbcb63e0609f0d268d2dd52c2ae86c9e2
Alex <av2o26@outlook.com>
2025-04-25 13:15:08 -0700

    Idle Timer Change

--- CST-323_CLC/Program.cs
+++ CST-323_CLC/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddSession(options =>
 {
     // Logout after 5 minutes of idle time
-    options.IdleTimeout = TimeSpan.FromMinutes(1);
+    options.IdleTimeout = TimeSpan.FromMinutes(5);
 });
 
 var app = builder.Build();
